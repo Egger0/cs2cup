c617d3c730366314c78562695db5c4000d742c56
m1ngsama <contact@m1ng.space>
2026-09-03 02:34:52 +0800

    feat: surface the current tournament on the homepage

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,11 +2,15 @@ import { HomeEvidence } from '@/components/home/HomeEvidence'
 import { HomeHero } from '@/components/home/HomeHero'
 import { HomeIndex } from '@/components/home/HomeIndex'
 import { HomeReveal } from '@/components/home/HomeReveal'
+import { homeTournamentSignal } from '@/lib/home-tournament-signal'
+import { getCurrentTournament, safely } from '@/lib/queries/public'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const tournament = await safely(getCurrentTournament, null)
+
   return (
     <>
-      <HomeHero />
+      <HomeHero signal={homeTournamentSignal(tournament)} />
       <HomeReveal />
       <HomeEvidence />
       <HomeIndex />
